b5602ba0fae2ce59f9e42185b947686a6c8f605a
Viktor Avelino <viktor.avelino@thebioagency.com>
2022-10-04 12:26:48 -0400

    Change result item to link

--- src/components/molecules/searchInput/atoms/searchResultItem/SearchResultItem.jsx
+++ src/components/molecules/searchInput/atoms/searchResultItem/SearchResultItem.jsx
@@ -3,7 +3,7 @@ import './SearchResultItem.scss';
 
 export const SearchResultItem = () => {
   return (
-    <div className="search-input__results__item">
+    <a className="search-input__results__item" href="#">
       <Avatar url="https://img.cdn4dd.com/p/fit=contain,width=100,height=100,format=auto,quality=95/media/restaurant/cover_square/Screen_Shot_2020-06-25_at_3.11.26_PM.png" />
 
       <div className="search-input__results__item__body">
@@ -11,6 +11,6 @@ export const SearchResultItem = () => {
         <p className="description">Burgers, Chicken, Wrap, Fish, Bakery</p>
         <p>Closed • Opens at 11 pm</p>
       </div>
-    </div>
+    </a>
   );
 };
