c9e44612571935e44df882e49b88223a845336ee
Viktor Avelino <viktor.avelino@gmail.com>
2022-10-14 14:40:03 -0400

    refactor: button

--- src/components/atoms/button/Button.jsx
+++ src/components/atoms/button/Button.jsx
@@ -1,9 +1,18 @@
+import { forwardRef, memo } from 'react';
+import { classNames } from '../../../lib/classNames';
+
 import './Button.scss';
 
-export const Button = ({ children, ...rest }) => {
+function ButtonComponent(
+  { children, className, icon: Icon = null, ...props },
+  ref
+) {
   return (
-    <button className="button" {...rest}>
+    <button {...props} className={classNames('btn', className)} ref={ref}>
+      {Boolean(Icon) && <Icon icon={Icon} height="100%" />}
       {children}
     </button>
   );
-};
+}
+
+export const Button = memo(forwardRef(ButtonComponent));
