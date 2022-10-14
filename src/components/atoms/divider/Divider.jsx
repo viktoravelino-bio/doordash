1180d8dabd5f4d5cb219130132b7203e96e08d32
Viktor Avelino <viktor.avelino@gmail.com>
2022-10-14 14:47:55 -0400

    refactor: divider

--- src/components/atoms/divider/Divider.jsx
+++ src/components/atoms/divider/Divider.jsx
@@ -1,23 +1,10 @@
+import { classNames } from '../../../lib/classNames';
 import './Divider.scss';
 
-/**
- * @typedef {Object} DividerProps
- * @property {string} [label]
- * @property {string} [className]
- * @property {string} [labelClassName]
- *
- */
-
-/**
- *
- * @param {DividerProps} {@link DividerProps}
- * @returns {JSX.Element}
- */
-export function Divider({ className = '', label, labelClassName }) {
-  // return <hr className={`divider ${className}`} {...rest} />;
+export function Divider({ className, label, ...props }) {
   return (
-    <div className={`divider ${className}`}>
-      {label && <span className={`${labelClassName}`}>{label}</span>}
+    <div {...props} className={classNames('divider', className)}>
+      {label && <span>{label}</span>}
     </div>
   );
 }
