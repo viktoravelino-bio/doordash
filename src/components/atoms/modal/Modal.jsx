270878caadc0ecb6690702d14214365b3eb038a9
Viktor Avelino <viktor.avelino@thebioagency.com>
2022-10-17 16:20:26 -0400

    component: modal

---
+++ src/components/atoms/modal/Modal.jsx
@@ -0,0 +1,46 @@
+import { createPortal } from 'react-dom';
+import { classNames } from '../../../lib/classNames';
+import './Modal.scss';
+
+function ModalHeader(props) {
+  return <header {...props} />;
+}
+
+function ModalBody(props) {
+  return <div className="body" {...props} />;
+}
+
+function ModalFooter(props) {
+  return <footer {...props} />;
+}
+
+function ModalContainer({ children, className, onClose, fullScreen, visible }) {
+  return (
+    <div
+      className={classNames('modal__container', className, {
+        'full-screen': Boolean(fullScreen),
+        visible: Boolean(visible),
+      })}
+      onClick={onClose}
+    >
+      <div
+        className={classNames('modal__content')}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+// export const Modal = (props) =>
+//   createPortal(
+//     <ModalContainer {...props} />,
+//     document.getElementById('portal-root')
+//   );
+
+export const Modal = Object.assign(ModalContainer, {
+  Header: ModalHeader,
+  Body: ModalBody,
+  Footer: ModalFooter,
+});
