61394511cc6f1397999825e5701e3bbfe7544fe9
Viktor Avelino <viktor.avelino@thebioagency.com>
2022-10-31 16:17:11 -0400

    changed menu data

--- src/assets/data/menuData.js
+++ src/assets/data/menuData.js
@@ -57,4 +57,120 @@ export const menuData = [
       },
     ],
   },
+  {
+    title: 'Burgers',
+    items: [
+      {
+        id: 1,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+      {
+        id: 2,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+      {
+        id: 3,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+    ],
+  },
+  {
+    title: 'Desserts',
+    items: [
+      {
+        id: 1,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+      {
+        id: 2,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+      {
+        id: 3,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+    ],
+  },
+  {
+    title: 'Drinks',
+    items: [
+      {
+        id: 1,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+      {
+        id: 2,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+      {
+        id: 3,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+    ],
+  },
+  {
+    title: 'Wraps',
+    items: [
+      {
+        id: 1,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+      {
+        id: 2,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+      {
+        id: 3,
+        name: 'World Famous Fries',
+        description:
+          'Egg, cheese, and Canadian bacon on a toasted English muffin',
+        price: 3.99,
+        img: 'https://img.cdn4dd.com/p/fit=cover,width=1200,height=1200,format=auto,quality=50/media/photosV2/42d5ab1b-ab2e-45eb-b1fc-79eee40cfd5a-retina-large.jpg ',
+      },
+    ],
+  },
 ];
