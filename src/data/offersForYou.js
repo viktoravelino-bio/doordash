85757e8bfff057e95425d0bee3d35b4d4f0cd9ab
Viktor Avelino <viktor.avelino@thebioagency.com>
2023-05-04 10:37:21 -0400

    router fix

--- src/data/offersForYou.js
+++ src/data/offersForYou.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default [
   {
+    id:1,
     restaurantName: 'Flaming Stove',
     isDashPass: true,
     highlight: '$0 delivery Fee',
@@ -8,6 +9,7 @@ export default [
       'https://img.cdn4dd.com/p/fit=cover,width=1200,format=jpeg,quality=50/media/store/header/951b7f95-0e54-4a88-8068-8066b17ab69e.jpg',
   },
   {
+    id:2,
     restaurantName: "Albert's Real Jamaican Foods",
     highlight: '$0 delivery Fee',
     isDashPass: false,
@@ -15,6 +17,7 @@ export default [
       'https://img.cdn4dd.com/p/fit=cover,width=1200,format=jpeg,quality=50/media/store/header/86538ea0-b7d5-4430-ba15-009d95fcb7e6.png',
   },
   {
+    id:3,
     restaurantName: 'Bread & Butter',
     highlight: '$0 delivery Fee',
     isDashPass: true,
